4ae38fe58396d139361cd9fa293281e20599fc45
atriorebwork-max <atriorebwork@gmail.com>
2026-01-06 13:09:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ CORS(
 # ------------------
 # GROQ CLIENT
 # ------------------
-GROQ_API_KEY = os.getenv("PTK_API_KK")
+GROQ_API_KEY = os.getenv("PTK_API_K")
 if not GROQ_API_KEY:
     raise RuntimeError("PTK_API_K not set")
 
@@ -165,4 +165,5 @@ def chat():
     return jsonify({
         "reply": response.choices[0].message.content
     })
+
 
