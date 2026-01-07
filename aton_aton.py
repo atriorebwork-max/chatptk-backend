024cfbb278c9ff83fb65dcb9feec5012d9d4d140
atriorebwork-max <atriorebwork@gmail.com>
2026-01-07 22:14:41 +0700

    Update aton_aton.py

--- aton_aton.py
+++ aton_aton.py
@@ -4,12 +4,14 @@ def marites(text):
         "who built you", "who coded", "creator", "made this program",
         "who designed", "who invented", "who authored", "Did openai", "did openai",
         "did chatgpt", "did this program", "did Meta", "Meta created", "Meta programmed", "made you",
-        "create you", "created you", "wo made", "wo made you", "Who programmed you"
+        "create you", "created you", "wo made", "wo made you", "Who programmed you",
+        "Programmed"
     ]
     text = text.lower()
 
 
     return any(k in text for k in keywords)
+
 
 
 
