e452d4a06b068fbd86e9bdaa352f3f35ec5e34b1
atriorebwork-max <atriorebwork@gmail.com>
2026-01-07 22:58:05 +0700

    Update aton_aton.py

--- aton_aton.py
+++ aton_aton.py
@@ -6,12 +6,14 @@ def marites(text):
         "did chatgpt", "did this program", "did Meta", "Meta created", "Meta programmed", "made you",
         "create you", "created you", "wo made", "wo made you", "Who programmed you",
         "Programmed", "who program", "wo programmed", "program", "Are you LLAMA programed",
-        "Did LLAMA programmed you", "What is your base ai model", "What ai model are you"
+        "Did LLAMA programmed you", "What is your base ai model", "What ai model are you",
+        "is your base ai model META", "is your base ai model PaLM 2", "are you made by (Population-Based Large Language Model 2)"
     ]
     text = text.lower()
 
 
     return any(k in text for k in keywords)
+
 
 
 
